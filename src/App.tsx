35ca04987f673c80811a2f44a7a2cab88316e61d
Cow-a-banga <chiruhind@gmail.com>
2026-03-29 13:56:34 +0500

    Fix fetch path for index2.json

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
     const [query, setQuery] = useState("");
 
     useEffect(() => {
-        fetch("/index2.json")
+        fetch("./index2.json")
             .then((res) => res.json())
             .then(setStreams);
     }, []);
