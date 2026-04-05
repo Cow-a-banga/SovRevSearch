30675535805a4e1371de75ef2514459d61b50642
Dmitry Chiruhin <chiruhind@gmail.com>
2026-04-06 02:19:09 +0500

    Добавлены фильтры

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState, useMemo } from "react";
-import { Input } from "antd";
+import {Input, Select} from "antd";
 import type { StreamModel } from "./models/streamModel.ts";
 import { StreamCard } from "./components/StreamCard.tsx";
 import Fuse from "fuse.js";
@@ -7,6 +7,9 @@ import Fuse from "fuse.js";
 function App() {
     const [streams, setStreams] = useState<StreamModel[]>([]);
     const [query, setQuery] = useState("");
+
+    const [selectedAuthors, setSelectedAuthors] = useState<string[]>([]);
+    const [selectedTypes, setSelectedTypes] = useState<string[]>([]);
 
     useEffect(() => {
         fetch("./data.json")
@@ -17,6 +20,16 @@ function App() {
             .then(setStreams);
     }, []);
 
+    const authors = useMemo(() => {
+        const set = new Set<string>();
+        streams.forEach(s => s.video.forEach(v => set.add(v.author)));
+        return Array.from(set).sort();
+    }, [streams]);
+
+    const types = useMemo(() => {
+        return Array.from(new Set(streams.map(s => s.type)));
+    }, [streams]);
+
     // создаём Fuse один раз при изменении streams
     const fuse = useMemo(() => {
         return new Fuse(streams, {
@@ -25,7 +38,23 @@ function App() {
         });
     }, [streams]);
 
-    const filteredStreams = query ? fuse.search(query).map((r) => r.item) : streams;
+    const filteredStreams = useMemo(() => {
+        let result = streams;
+
+        if(query.length > 0){
+            result = fuse.search(query).map(r => r.item)
+        }
+
+        if(selectedTypes.length > 0){
+            result = result.filter(x => selectedTypes.includes(x.type))
+        }
+
+        if(selectedAuthors.length > 0){
+            result = result.filter(x => x.video.some(v => selectedAuthors.includes(v.author)))
+        }
+
+        return result;
+    }, [streams, selectedAuthors, selectedTypes, query, fuse]);
 
     return (
         <div
@@ -49,6 +78,34 @@ function App() {
                     onChange={(e) => setQuery(e.target.value)}
                     style={{ marginBottom: 24 }}
                 />
+
+                {/* 🎛 Фильтры */}
+                <div style={{ display: "flex", gap: 12, marginBottom: 24 }}>
+
+                    {/* Авторы */}
+                    <Select
+                        mode="multiple"
+                        allowClear
+                        placeholder="Фильтр по авторам"
+                        style={{ flex: 1 }}
+                        options={authors.map(a => ({ label: a, value: a }))}
+                        value={selectedAuthors}
+                        onChange={setSelectedAuthors}
+                        showSearch
+                        optionFilterProp="label"
+                    />
+
+                    {/* Типы */}
+                    <Select
+                        mode="multiple"
+                        allowClear
+                        placeholder="Фильтр по типу"
+                        style={{ width: 400 }}
+                        options={types.map(t => ({ label: t, value: t }))}
+                        value={selectedTypes}
+                        onChange={setSelectedTypes}
+                    />
+                </div>
 
                 {/* Сетка карточек */}
                 <div
